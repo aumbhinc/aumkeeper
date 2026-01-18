8cfa3b170482d139a04a3852be3748324baf855f
aumbhinc <aumbhub@gmail.com>
2026-01-18 13:10:12 -0800

    Update templates and fix content blocks

--- api/handlers/home.go
+++ api/handlers/home.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"aumkeeper/api/viewdata"
+	"bytes"
 	"html/template"
 	"net/http"
 	"time"
@@ -10,14 +11,14 @@ import (
 func HomeHandler(t *template.Template) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		var content template.HTML
-		t.ExecuteTemplate(&content, "home_content", nil)
+		var buf bytes.Buffer
+		t.ExecuteTemplate(&buf, "home_content", nil)
 
 		data := viewdata.Layout{
 			Title:       "Home",
 			Description: "AumKeeper ERP platform for SMBs",
 			Year:        time.Now().Year(),
-			PageContent: content,
+			PageContent: template.HTML(buf.String()),
 		}
 
 		t.ExecuteTemplate(w, "base", data)
