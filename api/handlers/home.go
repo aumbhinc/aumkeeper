371d290b66415d3befbc62b2f96c6ecc21146657
aumbhinc <aumbhub@gmail.com>
2026-01-18 13:02:23 -0800

    Update templates and fix content blocks

--- api/handlers/home.go
+++ api/handlers/home.go
@@ -2,15 +2,25 @@ package handlers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 	"time"
 )
 
 func HomeHandler(t *template.Template) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		t.ExecuteTemplate(w, "home", map[string]any{
-			"Title": "Home",
-			"Year":  time.Now().Year(),
-		})
+
+		data := map[string]any{
+			"Title":       "Home",
+			"Year":        time.Now().Year(),
+			"PageContent": "home_content",
+		}
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+		if err := t.ExecuteTemplate(w, "base", data); err != nil {
+			log.Println("home render error:", err)
+			http.Error(w, "Template rendering error", http.StatusInternalServerError)
+		}
 	})
 }
