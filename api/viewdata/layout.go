766e712b31584bf4699ed118ecfe0e2015cdf286
aumbhinc <aumbhub@gmail.com>
2026-01-18 17:32:14 -0800

    Update templates and fix content blocks

--- api/viewdata/layout.go
+++ api/viewdata/layout.go
@@ -1,26 +1,32 @@
+// file: viewdata/viewdata.go
 package viewdata
 
 import "html/template"
 
-type StatCard struct {
+// Layout is the main page layout struct passed to base.html
+type Layout struct {
+	Title              string
+	Description        string
+	Year               int
+	PageContent        template.HTML
+	Stats              []Stat
+	RightPanelSections map[string][]RightPanelItem
+	IncludeDashboardJS bool
+	IncludeStaffJS     bool
+}
+
+// Stat represents each stat card in dashboard
+type Stat struct {
 	Name   string
 	Icon   string
 	Value  string
 	Change string
 }
 
-type FuncButton struct {
+// RightPanelItem represents a button in right panel
+type RightPanelItem struct {
 	Icon  string
 	Label string
 	Alert string
 	Value string
-}
-
-type Layout struct {
-	Title          string
-	Description    string
-	Year           int
-	PageContent    template.HTML
-	Stats          []StatCard
-	RightPanelSections map[string][]FuncButton
 }
